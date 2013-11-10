54f8910b0c6a376ae982d9aa1be9fbb81d142859
Mohammed Subhi <forsubhi@hotmail.com>
2013-11-10 15:32:06 +0300

    vollry

--- src/com/example/bugstracker/MainActivity.java
+++ src/com/example/bugstracker/MainActivity.java
@@ -1,9 +1,22 @@
 package com.example.bugstracker;
 
-import android.os.Bundle;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.example.bugstracker.network.MultipartRequest;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
@@ -58,6 +71,60 @@ public class MainActivity extends Activity {
 					Bitmap photo = (Bitmap) data.getExtras().get("data");
 					
 					ProductPhoto.setImageBitmap(photo);
+					
+					
+					ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+					photo.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+					//you can create a new file name "test.jpg" in sdcard folder.
+					File f = new File(Environment.getExternalStorageDirectory()
+					                        + File.separator + "test.jpg");
+					try {
+						f.createNewFile();
+						FileOutputStream fo = new FileOutputStream(f);
+						fo.write(bytes.toByteArray());
+
+						// remember close de FileOutput
+						fo.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					//write the bytes in file
+					
+					
+					ErrorListener el = new ErrorListener() {
+
+						@Override
+						public void onErrorResponse(VolleyError arg0) {
+							// TODO Auto-generated method stub
+							
+						}
+					};
+					
+					Listener<String> l  = new Listener<String>() {
+
+						@Override
+						public void onResponse(String arg0) {
+
+
+							
+							String x = arg0;
+							
+							x.toString();
+							
+						}
+					};
+					
+					MultipartRequest mr = new MultipartRequest("http://www.toast.net/mail/", el, l  , f, "1");
+					
+					
+					
+					RequestQueue queue = Volley.newRequestQueue(this);
+					
+					queue.add(mr);
+				
+					
 			}}
 		
 		
