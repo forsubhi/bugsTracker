d1799c0fef66ae92bb105e25fc880487c5077286
Mohammed Subhi <forsubhi@hotmail.com>
2013-11-10 14:26:24 +0300

    take photo
    take photo

--- src/com/example/bugstracker/MainActivity.java
+++ src/com/example/bugstracker/MainActivity.java
@@ -2,14 +2,35 @@ package com.example.bugstracker;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+	private View TakePhotoBtn;
+
+	private ImageView ProductPhoto;
+
+	private static final int CAMERA_REQUEST = 1888;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TakePhotoBtn  =  findViewById(R.id.TakePhotoBtn);
+		ProductPhoto = (ImageView)   findViewById(R.id.ProductPhoto);
+		
+		
+		TakePhotoBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			TakePhoto();
+				
+			}
+		});
 	}
 
 	@Override
@@ -18,5 +39,28 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	
+	
+	void TakePhoto() {
+
+		Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+			if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+				
+					Bitmap photo = (Bitmap) data.getExtras().get("data");
+					
+					ProductPhoto.setImageBitmap(photo);
+			}}
+		
+		
+		
 
 }
